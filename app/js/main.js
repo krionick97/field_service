1fa9c3697d23d5eb35ccd56205d73fd0a6ea097d
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-12-20 01:11:49 +0300

    burger-button

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const burgerButton = document.querySelector('.header__account-navigate_burger');
+
+burgerButton.addEventListener('click', function() {
+  if (burgerButton.classList.contains('header__account-navigate_burger-active')) {
+    burgerButton.classList.remove('header__account-navigate_burger-active');
+  }
+  else { burgerButton.classList.add('header__account-navigate_burger-active'); }
+});
