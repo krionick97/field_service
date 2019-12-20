5968c34f5d527e3287e2a6f2f4663ac7ab0e003c
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-12-21 02:15:00 +0300

    account page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,33 @@
 'use strict'
 
 const burgerButton = document.querySelector('.header__account-navigate_burger');
+const markerButtons = document.querySelectorAll('.header__account-navigate_marker');
 
+/* Burger Arrow Click */
 burgerButton.addEventListener('click', function() {
-  if (burgerButton.classList.contains('header__account-navigate_burger-active')) {
-    burgerButton.classList.remove('header__account-navigate_burger-active');
+  if (burgerButton.classList.contains('active')) {
+    burgerButton.classList.remove('active');
   }
-  else { burgerButton.classList.add('header__account-navigate_burger-active'); }
+  else { burgerButton.classList.add('active'); }
 });
+/* ------------------------------------------------- */
+
+/* Marker Buttons Clicking */
+for (let i = 0; i < markerButtons.length; i++) {
+  markerButtons[i].addEventListener('click', function() {
+    if (!markerButtons[i].classList.contains('active')) {
+      markerButtons[i].classList.add('active');
+    }
+    for (let j = i - 1; j >= 0; j--) {
+          if (markerButtons[j].classList.contains('active')) {
+        markerButtons[j].classList.remove('active');
+      }
+    }
+    for (let k = i + 1; k < markerButtons.length; k++) {
+          if (markerButtons[k].classList.contains('active')) {
+        markerButtons[k].classList.remove('active');
+      }
+    }
+  });
+}
+/* -------------------------------------------------------- */
