d6316ee6026b8c704f15a13bf55f72b6297872a8
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-12-21 02:55:32 +0300

    burger clickink menu active

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,8 +2,10 @@
 
 const burgerButton = document.querySelector('.header__account-navigate_burger');
 const markerButtons = document.querySelectorAll('.header__account-navigate_marker');
+const accountMenu = document.querySelector('.section__account-menu');
+const menuItems = document.querySelectorAll('.section__account-menu_item');
 
-/* Burger Arrow Click */
+/* Burger Arrow Click Arrows */
 burgerButton.addEventListener('click', function() {
   if (burgerButton.classList.contains('active')) {
     burgerButton.classList.remove('active');
@@ -12,22 +14,41 @@ burgerButton.addEventListener('click', function() {
 });
 /* ------------------------------------------------- */
 
-/* Marker Buttons Clicking */
+/* Marker Buttons Clicking Active */
 for (let i = 0; i < markerButtons.length; i++) {
   markerButtons[i].addEventListener('click', function() {
-    if (!markerButtons[i].classList.contains('active')) {
-      markerButtons[i].classList.add('active');
-    }
+    if (!markerButtons[i].classList.contains('active')) markerButtons[i].classList.add('active');
     for (let j = i - 1; j >= 0; j--) {
-          if (markerButtons[j].classList.contains('active')) {
-        markerButtons[j].classList.remove('active');
-      }
+      if (markerButtons[j].classList.contains('active')) markerButtons[j].classList.remove('active');
     }
     for (let k = i + 1; k < markerButtons.length; k++) {
-          if (markerButtons[k].classList.contains('active')) {
-        markerButtons[k].classList.remove('active');
-      }
+      if (markerButtons[k].classList.contains('active')) markerButtons[k].classList.remove('active');
     }
   });
 }
 /* -------------------------------------------------------- */
+
+/* Menu Item Button Clicking Active */
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('click', function() {
+    if (!menuItems[i].classList.contains('active')) menuItems[i].classList.add('active');
+    for (let j = i - 1; j >=0; j--) {
+      if (menuItems[j].classList.contains('active')) menuItems[j].classList.remove('active');
+    }
+    for (let k = i + 1; k < menuItems.length; k++) {
+      if (menuItems[k].classList.contains('active')) menuItems[k].classList.remove('active')
+    }
+  });
+}
+/* ---------------------------------------------------------- */
+
+/* Burger Button Menu Active */
+burgerButton.addEventListener('click', function() {
+  if (burgerButton.classList.contains('active')) { accountMenu.classList.add('active'); }
+  else { accountMenu.classList.remove('active'); }
+});
+
+
+
+
+/* ---------------------------------------------------------- */
