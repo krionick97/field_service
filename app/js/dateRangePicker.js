4ea5a7a9e9d5ba5da90ce44cd445e6a0bd28f18a
Vlad_Kuznetcov <mistermers8@gmail.com>
2020-04-15 21:26:19 +0300

    corrections

--- app/js/dateRangePicker.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-let date = new Date();
-let day = date.getDate();
-let weekDay = date.getDay();
-let month = date.getMonth();
-let year = date.getFullYear();
-let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
